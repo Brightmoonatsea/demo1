4813252abcd320fc1d05afd24c1de88ebe567c0d
cjm1218 <1150242343@qq.com>
2020-02-11 16:03:38 +0800

    注册模型 生成迁移文件 执行迁移

--- end/project1/bookdemo/booktest/models.py
+++ end/project1/bookdemo/booktest/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+
+
+# 每一张表就是一个模型类
+# 有了ORM后我们就可以定义表对应的模型类
+class Book(models.Model):
+    """
+    book 继承了Model类  因为Model拥有操作数据库的功能
+    """
+    title = models.CharField(max_length=20)
+    pub_date = models.DateField(default="1983-06-01")
+
+
+class Hero(models.Model):
+    """
+    hero 类继承了Model类
+    """
+
+    name = models.CharField(max_length=20)
+    gender = models.CharField(max_length=6, choices=(('male', '男'), ('female', '女')), default='male')
+    content = models.CharField(max_length=100)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
