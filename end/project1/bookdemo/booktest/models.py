ff23f046dd29cc99ea8dff3099a8cac796749b7e
cjm1218 <1150242343@qq.com>
2020-02-11 17:56:00 +0800

    执行迁移 测试数据 创建超级用户 创建后台管理

--- end/project1/bookdemo/booktest/models.py
+++ end/project1/bookdemo/booktest/models.py
@@ -13,6 +13,9 @@ class Book(models.Model):
     price = models.FloatField(default=0)
     pub_date = models.DateField(default="1983-06-01")
 
+    def __str__(self):
+        return self.title
+
 
 class Hero(models.Model):
     """
@@ -23,3 +26,12 @@ class Hero(models.Model):
     gender = models.CharField(max_length=6, choices=(('male', '男'), ('female', '女')), default='male')
     content = models.CharField(max_length=100)
     book = models.ForeignKey(Book, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+# django orm关联查询
+# 多方Hero   一方Book
+# 1多找一， 多方对象.关系字段    exp: h1.book
+# 2一找多， 一方对象.小写多方类名_set.all()   exp:  b1.hero_set.all()
