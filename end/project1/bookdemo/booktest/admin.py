99227b86d3ab25888251f575d3b3388b5a6910b5
cjm1218 <1150242343@qq.com>
2020-02-13 11:39:09 +0800

    enddata

--- end/project1/bookdemo/booktest/admin.py
+++ end/project1/bookdemo/booktest/admin.py
@@ -1,7 +1,34 @@
 from django.contrib import admin
 
+
 # Register your models here.
 
 from .models import Book, Hero
-admin.site.register(Book)
-admin.site.register(Hero)
+
+
+class HeroInline(admin.StackedInline):
+    model = Hero
+    extra = 1
+
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'price', 'pub_date')
+    list_filter = ['title']
+    search_fields = ['title', 'price', 'pub_date']
+    list_per_page = 100
+    inlines = [HeroInline]
+
+
+admin.site.register(Book, BookAdmin)
+
+
+class HeroAdmin(admin.ModelAdmin):
+    list_display = ('name', 'gender', 'content', 'book')
+    list_filter = ['name', 'gender']
+    search_fields = ['gender', 'name']
+    list_per_page = 100
+
+
+admin.site.register(Hero, HeroAdmin)
+
+
